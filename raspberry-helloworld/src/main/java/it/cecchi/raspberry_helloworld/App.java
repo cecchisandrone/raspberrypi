b031808a332b9e0503f011676492de8084027c90
cecchisandrone@gmail.com <cecchisandrone@gmail.com>
2013-12-01 17:03:02 +0100

    1st commit

---
+++ raspberry-helloworld/src/main/java/it/cecchi/raspberry_helloworld/App.java
@@ -0,0 +1,13 @@
+package it.cecchi.raspberry_helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
