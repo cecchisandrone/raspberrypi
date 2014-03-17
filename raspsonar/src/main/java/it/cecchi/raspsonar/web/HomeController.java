196a93b9628524c4c10999b526836eed3304725b
cecchisandrone@gmail.com <cecchisandrone@gmail.com>
2014-03-17 21:50:35 +0100

    Spring MVC configuration

---
+++ raspsonar/src/main/java/it/cecchi/raspsonar/web/HomeController.java
@@ -0,0 +1,17 @@
+package it.cecchi.raspsonar.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String home() {
+        System.out.println("HomeController: Passing through...");
+        return "views/home.jsp";
+    }
+}
