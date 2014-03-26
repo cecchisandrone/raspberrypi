ed417c4389834bb624647f02b7a8e022f1157483
a.dionisi <a.dionisi@PCH329.ids.intranet>
2014-03-26 13:00:44 +0100

    Implemented REST service for raspsonar

--- smarthome/raspsonar/src/main/java/it/cecchi/raspsonar/service/SonarService.java
+++ smarthome/raspsonar/src/main/java/it/cecchi/raspsonar/service/SonarService.java
@@ -1,22 +1,32 @@
-
 package it.cecchi.raspsonar.service;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-/** Example resource class hosted at the URI path "/myresource"
+/**
+ * Example resource class hosted at the URI path "/myresource"
  */
-@Path("/myresource")
+@Path("/sonarservice")
 public class SonarService {
-    
-    /** Method processing HTTP GET requests, producing "text/plain" MIME media
-     * type.
-     * @return String that will be send back as a response of type "text/plain".
-     */
-    @GET 
-    @Produces("text/plain")
-    public String getIt() {
-        return "Hi there!";
-    }
+
+	/**
+	 * Method processing HTTP GET requests, producing "text/plain" MIME media type.
+	 * 
+	 * @return Double that will be send back as a response of type "text/plain".
+	 * @throws Exception
+	 */
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/distance")
+	public double getDistance() throws Exception {
+
+		try {
+			double range = SonarSensor.getInstance().getRange();
+			return range;
+		} catch (UnsatisfiedLinkError e) {
+			throw e;
+		}
+	}
 }
--- smarthome/raspsonar/src/main/webapp/index.jsp
+++ smarthome/raspsonar/src/main/webapp/index.jsp
@@ -1,8 +1,6 @@
 <html>
 <body>
 <h2>Jersey RESTful Web Application!</h2>
-<p><a href="webresources/myresource">Jersey resource</a>
-<p>Visit the <a href="http://jersey.java.net">Project Jersey website</a>
-for more information on Jersey!
+<p><a href="rest/sonarservice/distance">Get distance</a>
 </body>
 </html>
