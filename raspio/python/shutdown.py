71d2e0aed8ff8ab11a9f87f1c06ffe5a7969b8bf
cecchisandrone <cecchisandrone@gmail.com>
2015-02-24 08:14:55 +0000

    Added shutdown script

---
+++ raspio/python/shutdown.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+	GPIO.wait_for_edge(18, GPIO.FALLING)
+ 	print('Executing shutdown...')
+        os.system("sudo shutdown now");
+
+except KeyboardInterrupt:        
+	GPIO.cleanup()
+
+
+
