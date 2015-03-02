de62d128cc94a0841020e27652613be5a8cecaaf
cecchisandrone <cecchisandrone@gmail.com>
2015-03-02 03:54:23 +0000

    Added -h in shutdown command

--- raspio/python/shutdown.py
+++ raspio/python/shutdown.py
@@ -9,7 +9,7 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 try:
 	GPIO.wait_for_edge(18, GPIO.FALLING)
  	print('Executing shutdown...')
-        os.system("sudo shutdown now");
+        os.system("sudo shutdown -h now");
 
 except KeyboardInterrupt:        
 	GPIO.cleanup()
