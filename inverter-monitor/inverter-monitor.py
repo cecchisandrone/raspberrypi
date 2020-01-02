f9373df46dad68ec9b6f1276a376000f25a394e4
Alessandro Dionisi <cecchisandrone@gmail.com>
2020-01-02 10:06:47 +0100

    Implemented metrics enpoint

--- inverter-monitor/inverter-monitor.py
+++ inverter-monitor/inverter-monitor.py
@@ -4,7 +4,7 @@ from flask import jsonify
 from time import sleep
 import subprocess
 
-cmd = ["aurora -a 2 -D -d 0 -e /dev/ttyUSB0 | cut -f2 -d '=' | tr -s '\n' '\n'"]
+cmd = ["bash -o pipefail -c \"aurora -a 2 -D -d 0 -e /dev/ttyUSB0 | cut -f2 -d '=' | tr -s '\n' '\n'\""]
 fields = {
             0: "input_1_voltage",
             1: "input_1_current",
@@ -22,23 +22,24 @@ fields = {
             13: "daily_energy",
             14: "weekly_energy",
             15: "monthly_energy",
-            16: "total_energy",
-            17: "partial_energy",
-            18: "vbulk",
-            19: "riso",
-            20: "power_peak_today",
-            21: "power_pin_1",
-            22: "power_pin_2",
-            23: "vbulk_mid",
-            24: "vbulk_dc_dc",
-            25: "ileak_dc_dc_reading",
-            26: "ileak_inverter_reading",
-            27: "grid_voltage_dc_dc",
-            28: "grid_voltage_avg",
-            29: "grid_voltage_neutral",
-            30: "grid_frequency_dc_dc",
-            31: "power_peak",
-            32: "wind_generator_frequency"
+            16: "yearly_energy",
+            17: "total_energy",
+            18: "partial_energy",
+            19: "vbulk",
+            20: "riso",
+            21: "power_peak_today",
+            22: "power_pin_1",
+            23: "power_pin_2",
+            24: "vbulk_mid",
+            25: "vbulk_dc_dc",
+            26: "ileak_dc_dc_reading",
+            27: "ileak_inverter_reading",
+            28: "grid_voltage_dc_dc",
+            29: "grid_voltage_avg",
+            30: "grid_voltage_neutral",
+            31: "grid_frequency_dc_dc",
+            32: "power_peak",
+            33: "wind_generator_frequency"
          }
 
 app = Flask(__name__)
@@ -49,16 +50,14 @@ def metrics_endpoint():
   try:
     output = subprocess.check_output(cmd, shell=True)
     output = output.replace("Extended DSP Reporting",'\n').splitlines()
-    del output[0]
-    print(output)
+    # Delete empty lines (iterate backwards)
+    for i in range(len(output) - 1, -1, -1):
+      if output[i] == '':
+        del(output[i])
     if len(output) != len(fields):
-      print(len(output))
-      print(len(fields))
-      #return "bad reading", 500
+      return "Bad reading", 500
     for i in range(len(output)):
       number = output[i].lstrip().split(" ")[0]
-      print(number)
-      print(fields[i])
       if i in fields:
         json[fields[i]] = number
   except subprocess.CalledProcessError as e:
@@ -68,10 +67,8 @@ def metrics_endpoint():
   except OSError as e:
     print e
     return "error", 500
-  print json
   return jsonify(json), 200
 
 if __name__ == '__main__':
-  metrics_endpoint()
   app.run(host='0.0.0.0', port=6000)
 
